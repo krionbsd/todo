09222492e9391872625272e47464e042d284cd0b
krionbsd <krion@FreeBSD.org>
2021-11-09 10:00:33 +0100

    Pass ts_seconds from chrono to serde

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,11 @@
-use chrono::{DateTime, Utc};
+use chrono::{ts_seconds, DateTime, Local, Utc};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub timestamp: DateTime<Utc>,
 }
 
